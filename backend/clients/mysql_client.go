052908be696ef0e17d2088be3d61bf2345ba3fe8
agus gonzalez <2313442@ucc.edu.ar>
2025-05-26 11:41:57 -0300

    ..

--- backend/clients/mysql_client.go
+++ backend/clients/mysql_client.go
@@ -12,16 +12,16 @@ var (
 )
 
 func init() {
-	user := "root"
+	/*user := "root"
 	password := "Agus2025uccBD-"
 	host := "localhost"
 	port := 3306
-	database := "backend"
-	/*user := "root"
+	database := "backend"*/
+	user := "root"
 	password := "17122004Ff"
 	host := "localhost"
 	port := 3306
-	database := "backend"*/
+	database := "backend"
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%d)/%s?parseTime=true&charset=utf8mb4&loc=Local",
 		user, password, host, port, database)
@@ -32,88 +32,88 @@ func init() {
 		panic(fmt.Sprintf("error connecting to DB: %v", err))
 	}
 
-	/*DB.AutoMigrate(&dao.Usuario{})
-	DB.AutoMigrate(&dao.Horario{})
-	DB.AutoMigrate(&dao.ActDeportiva{}) //crea tablas en la base de datos
-	DB.AutoMigrate(&dao.Inscripcion{})
-	DB.AutoMigrate(&dao.Categoria{})
+	/*	DB.AutoMigrate(&dao.Usuario{})
+		DB.AutoMigrate(&dao.Horario{})
+		DB.AutoMigrate(&dao.ActDeportiva{}) //crea tablas en la base de datos
+		DB.AutoMigrate(&dao.Inscripcion{})
+		DB.AutoMigrate(&dao.Categoria{})
 
-	DB.Create(&dao.Usuario{
-		NombreUsuario:   "mateo123",
-		ContraseniaHash: Utils.HashSHA256("mateo"),
-		Nombre:          "mateo",
-		Apellido:        "Alfei",
-		DNI:             43928426,
-		Mail:            "mateo123@gmail.com",
-		IsAdmin:         false,
-	})
+		DB.Create(&dao.Usuario{
+			NombreUsuario:   "mateo123",
+			ContraseniaHash: Utils.HashSHA256("mateo"),
+			Nombre:          "mateo",
+			Apellido:        "Alfei",
+			DNI:             43928426,
+			Mail:            "mateo123@gmail.com",
+			IsAdmin:         false,
+		})
 
-	DB.Create(&dao.ActDeportiva{
-		Nombre:         "Spinning",
-		NombreProfesor: "Emiliano",
-		Cupos:          10,
-		IdCategoria:    1,
-		Horarios: []dao.Horario{
-			{
-				Dia:           "Viernes",
-				HorarioInicio: "14:00",
-				HorarioFin:    "15:00",
+		DB.Create(&dao.ActDeportiva{
+			Nombre:         "Spinning",
+			NombreProfesor: "Emiliano",
+			Cupos:          10,
+			IdCategoria:    1,
+			Horarios: []dao.Horario{
+				{
+						Dia:           "Martes",
+						HorarioInicio: "18:00",
+						HorarioFin:    "20:00",
+				},
+				{
+					Dia:           "Viernes",
+					HorarioInicio: "14:00",
+					HorarioFin:    "15:00",
+				},
 			},
-			{
-				Dia:           "Martes",
-				HorarioInicio: "18:00",
-				HorarioFin:    "20:00",
+		})
+
+		DB.Create(&dao.ActDeportiva{
+			Nombre:         "Yoga",
+			NombreProfesor: "Juan",
+			Cupos:          10,
+			IdCategoria:    3,
+			Horarios: []dao.Horario{
+				{
+					Dia:           "Lunes",
+					HorarioInicio: "10:00",
+					HorarioFin:    "12:00",
+				},
+				{
+					Dia:           "Martes",
+					HorarioInicio: "10:00",
+					HorarioFin:    "12:00",
+				},
 			},
-		},
-	})
+		})
 
-	DB.Create(&dao.ActDeportiva{
-		Nombre:         "Yoga",
-		NombreProfesor: "Juan",
-		Cupos:          10,
-		IdCategoria:    3,
-		Horarios: []dao.Horario{
-			{
-				Dia:           "Lunes",
-				HorarioInicio: "10:00",
-				HorarioFin:    "12:00",
-			},
-			{
-				Dia:           "Lunes",
-				HorarioInicio: "10:00",
-				HorarioFin:    "12:00",
-			},
-		},
-	})
+		DB.Create(&dao.Usuario{
+			Nombre:          "Martina",
+			Apellido:        "Valdo",
+			NombreUsuario:   "Martina123456",
+			DNI:             46032879,
+			Mail:            "mmmm@gmail.com",
+			ContraseniaHash: Utils.HashSHA256("12345"),
+			IsAdmin:         true,
+		})
 
-	DB.Create(&dao.Usuario{
-		Nombre:          "Martina",
-		Apellido:        "Valdo",
-		NombreUsuario:   "Martina123456",
-		DNI:             46032879,
-		Mail:            "mmmm@gmail.com",
-		ContraseniaHash: Utils.HashSHA256("12345"),
-		IsAdmin:         true,
-	})
+		DB.Create(&dao.Categoria{
+			Nombre: "Musculacion",
+		})
 
-	DB.Create(&dao.Categoria{
-		Nombre: "Musculacion",
-	})
+		DB.Create(&dao.Categoria{
+			Nombre: "Funcional",
+		})
 
-	DB.Create(&dao.Categoria{
-		Nombre: "Funcional",
-	})
-
-	DB.Create(&dao.Inscripcion{
-		IdUsuario:   1,
-		IdActividad: 1,
-		IdHorario:   1,
-	})
-	DB.Create(&dao.Inscripcion{
-		IdUsuario:   1,
-		IdActividad: 2,
-		IdHorario:   2,
-	})*/
+		DB.Create(&dao.Inscripcion{
+			IdUsuario:   1,
+			IdActividad: 1,
+			IdHorario:   1,
+		})
+		DB.Create(&dao.Inscripcion{
+			IdUsuario:   1,
+			IdActividad: 2,
+			IdHorario:   2,
+		})*/
 }
 
 func GetUserByUsername(username string) (dao.Usuario, error) {
@@ -149,33 +149,48 @@ func GetHorariosByActividad(idActividad int) ([]dao.Horario, error) {
 	err := DB.Where("id_actividad = ?", idActividad).Find(&horarios).Error
 	return horarios, err
 }
-
-func GetActInscripcion(IDusuario int) ([]dao.ActDeportiva, error) {
+func GetActInscripcion(IDusuario int) ([]dao.ActDeportiva, []dao.Horario, error) {
 	var Inscripcion []dao.Inscripcion
-	txn := DB.Where("IDusuario = ?", IDusuario).Find(&Inscripcion).Error
-	if txn.Error != nil {
-		return []dao.ActDeportiva{}, fmt.Errorf("Error: el usuario no se encuentra inscripto %w", txn.Error)
+	var Horarios []dao.Horario
+	err := DB.Where("Id_usuario = ?", IDusuario).Find(&Inscripcion).Error
+	if err != nil {
+		return []dao.ActDeportiva{}, Horarios, fmt.Errorf("Error: el usuario no se encuentra inscripto %w", err)
 	}
-
 	var actividades []dao.ActDeportiva //lista que contendra las act a las que esta inscripto el usuario
 	for _, insc := range Inscripcion { //recorre la lista de inscripciones obtenida anteriormente
 		//insc es cada inscr individual
 		var actividad dao.ActDeportiva //cada actividad correspondiente a una inscripcion
-		if err := DB.First(&actividad, "actividad = ?", insc.IdActividad).Error; err == nil {
+		if err := DB.First(&actividad, "id_actividad = ?", insc.IdActividad).Error; err == nil {
 			actividades = append(actividades, actividad)
 		}
-
+		var horario dao.Horario
+		if err := DB.First(&horario, "id_horario = ?", insc.IdHorario).Error; err == nil {
+			Horarios = append(Horarios, horario)
+		}
 	}
-	return actividades, nil
+	return actividades, Horarios, nil
 }
 func GenerarInscripcion(IDuser int, IDact int, IDhorario int) error {
-	txn := DB.Create(&dao.Inscripcion{
-		IdUsuario:   IDuser,
-		IdActividad: IDact,
-		IdHorario:   IDhorario,
-	})
-	if txn.Error != nil {
-		return fmt.Errorf("Error: No se pudo realizar la inscripcion %w", txn.Error)
+	var actividad dao.ActDeportiva
+	if err := DB.First(&actividad, IDact).Error; err != nil {
+		return fmt.Errorf("Error: No se encontró la actividad: %w", err)
 	}
-	return nil
+	if actividad.Cupos <= 0 {
+		return fmt.Errorf("Error: No hay cupos disponibles para esta actividad")
+	}
+	err := DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&actividad).Where("cupos > 0").Update("cupos", gorm.Expr("cupos - ?", 1)).Error; err != nil {
+			return fmt.Errorf("Error al actualizar cupos: %w", err)
+		}
+		txn := DB.Create(&dao.Inscripcion{
+			IdUsuario:   IDuser,
+			IdActividad: IDact,
+			IdHorario:   IDhorario,
+		})
+		if txn.Error != nil {
+			return fmt.Errorf("Error: No se pudo realizar la inscripcion %w", txn.Error)
+		}
+		return nil
+	})
+	return err
 }
